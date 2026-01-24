44008775c3a8fc68ec2d56a828d5b0413ef7145c
JefeThePug <107787719+JefeThePug@users.noreply.github.com>
2026-01-24 14:49:37 +0900

    dynamically load css & border images
    dynamically load css + pre-load border images
    from globals (context_processor)

--- app/templating/globals.py
+++ app/templating/globals.py
@@ -1,3 +1,7 @@
+import os
+
+from flask import session
+
 from app.appctx import get_app
 
 
@@ -6,18 +10,20 @@ def register_globals():
     Attaches utility functions to the Flask application so they can be
     used directly inside Jinja templates.
     """
+    app = get_app()
 
-    @get_app().template_global()
+    @app.template_global()
     def obfuscate(year: str, value: str | int) -> str | int:
         """Obfuscate a value using the obfuscation mapping.
         Args:
             value (str | int): The value to obfuscate.
+            year (str): Current year
         Returns:
             str | int: The obfuscated value.
         """
-        return get_app().data_cache.admin.html_nums[year][value]
+        return app.data_cache.admin.html_nums[year][value]
 
-    @get_app().template_global()
+    @app.template_global()
     def obscure_post(value: str | int) -> str:
         """Obscure a challenge number for display in templates.
         Args:
@@ -27,4 +33,12 @@ def register_globals():
         """
         if isinstance(value, str):
             value = int(value)
-        return get_app().data_cache.admin.obfuscations[int(value)]
+        return app.data_cache.admin.obfuscations[int(value)]
+
+    @app.context_processor
+    def inject_css_files() -> dict[str, list[str]]:
+        year = app.config['CURRENT_YEAR']
+        return {
+            "borders": [f"{y}{p}" for y in range(2025, year + 1) for p in "AB"],
+            "css_files": ["sponsor", "navbar", "style", f"style{session.get('year', year)}"],
+        }
