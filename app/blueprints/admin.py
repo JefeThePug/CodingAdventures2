2481adcfab37d8e47eb120eca2ce955dd163e0d5
JefeThePug <107787719+JefeThePug@users.noreply.github.com>
2026-01-29 15:37:02 +0900

    more admin feature design

--- app/blueprints/admin.py
+++ app/blueprints/admin.py
@@ -1,4 +1,6 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, session, flash
+
+from app.appctx import get_app
 
 admin_bp = Blueprint("admin", __name__)
 
@@ -9,3 +11,38 @@ def admin():
 @admin_bp.route("/admin/home")
 def admin_home():
     return render_template("admin/home.html")
+
+@admin_bp.route("/admin/discord", methods=["GET", "POST"])
+def discord():
+    app = get_app()
+    years = list(map(str, range(2025, app.config["CURRENT_YEAR"] + 1)))
+    selected_year = request.args.get(
+        "year",
+        request.form.get("year", f"{app.config['CURRENT_YEAR']}")
+    )
+    channels = {
+        "2025": {i: "TEST-2025" for i in range(1, 11)},
+        "2026": {i: "TEST-2026" for i in range(1, 11)}
+    }
+    # flash("Invalid test", "error")
+
+    return render_template(
+        "admin/discord.html",
+        years=years,
+        selected_year=selected_year,
+        guild="GUILD",
+        role="ROLE",
+        channels=channels,
+    )
+
+@admin_bp.route("/admin/release", methods=["GET", "POST"])
+def release():
+    app = get_app()
+    years = list(map(str, range(2025, app.config["CURRENT_YEAR"] + 1)))
+    releases = {year: 10 for year in years}
+
+    return render_template(
+        "admin/release.html",
+        years=years,
+        release=releases,
+    )
