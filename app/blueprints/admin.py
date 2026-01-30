841ab98d67e0ad7e589637a89a932e8d990f8903
JefeThePug <107787719+JefeThePug@users.noreply.github.com>
2026-01-30 12:05:56 +0900

    more admin design progress

--- app/blueprints/admin.py
+++ app/blueprints/admin.py
@@ -46,3 +46,79 @@ def release():
         years=years,
         release=releases,
     )
+
+@admin_bp.route("/admin/solutions", methods=["GET", "POST"])
+def solutions():
+    app = get_app()
+    years = list(map(str, range(2025, app.config["CURRENT_YEAR"] + 1)))
+    selected_year = request.args.get(
+        "year",
+        request.form.get("year", f"{app.config['CURRENT_YEAR']}")
+    )
+    print(request.args.get("year", "no args"), request.form.get("year", "no form"))
+
+    solution_list = {
+        "2025": {i: [f"25 test-{i}0", f"25 test-{i}1"] for i in range(1, 11)},
+        "2026": {i: [f"26 test-{i}0", f"26 test-{i}1"] for i in range(1, 11)},
+    }
+
+    return render_template(
+        "admin/solutions.html",
+        years=years,
+        selected_year=selected_year,
+        solutions=solution_list,
+    )
+
+@admin_bp.route("/admin/html", methods=["GET", "POST"])
+def html():
+    app = get_app()
+    years = list(map(str, range(2025, app.config["CURRENT_YEAR"] + 1)))
+    print(request.args.get("year", "no args"), request.form.get("year", "no form"))
+    selected_year = request.args.get(
+        "year",
+        request.form.get("year", f"{app.config['CURRENT_YEAR']}")
+    )
+    selected_week = int(request.args.get("week", request.form.get("week", 1)))
+
+    data = {
+        "2025": {
+            i: { part: {
+                "title": f"2025.{i}.{part} title",
+                "content": f"2025.{i}.{part} content",
+                "instructions": f"2025.{i}.{part} instructions",
+                "input": f"2025.{i}.{part} input",
+                "form": f"2025.{i}.{part} form",
+                "solution": f"2025.{i}.{part} solution",
+            } for part in range(1, 3)
+            } for i in range(1, 11)
+        },
+        "2026": {
+            i: { part: {
+                "title": f"2026.{i}.{part} title",
+                "content": f"2026.{i}.{part} content",
+                "instructions": f"2026.{i}.{part} instructions",
+                "input": f"2026.{i}.{part} input",
+                "form": f"2026.{i}.{part} form",
+                "solution": f"2026.{i}.{part} solution",
+            } for part in range(1, 3)
+            } for i in range(1, 11)
+        },
+    }
+
+    return render_template(
+        "admin/html.html",
+        years=years,
+        selected_year=selected_year,
+        selected_week = selected_week,
+        data=data,
+    )
+
+@admin_bp.route("/admin/perms", methods=["GET", "POST"])
+def perms():
+    app = get_app()
+    perm_list = [f"TESTING{i}" for i in range(__import__("random").randint(3, 6))]
+
+    return render_template(
+        "admin/perms.html",
+        perms=perm_list,
+    )
